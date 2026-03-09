a27d44f2183d973ceb83b47948461cc969fc11b4
Nasir Joseph Guyton-Barreau <nasirjoseph.guyton@students.dominican.edu>
2026-03-08 19:45:43 -0700

    Added additional tests to Virus class

--- virus.py
+++ virus.py
@@ -7,9 +7,17 @@ class Virus(object):
 
 # Test this class
 if __name__ == "__main__":
-    # Test your virus class by making an instance and confirming 
-    # it has the attributes you defined
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+
+    virus2 = Virus("Flu", 0.4, 0.05)
+    assert virus2.name == "Flu"
+    assert virus2.repro_rate == 0.4
+    assert virus2.mortality_rate == 0.05
+
+    virus3 = Virus("Ebola", 0.25, 0.7)
+    assert virus3.name == "Ebola"
+    assert virus3.repro_rate == 0.25
+    assert virus3.mortality_rate == 0.7
